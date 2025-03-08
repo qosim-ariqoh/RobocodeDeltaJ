fa7d2a4e6260c1f67963c401193379b17b11dc9d
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2025-03-08 14:18:25 +0700

    Generate Robocode

--- RoboRobo/src/jab/module/SelectEnemy.java
+++ RoboRobo/src/jab/module/SelectEnemy.java
@@ -1,25 +1,33 @@
 package jab.module;
 
+import java.util.Iterator;
+import jab.module.BotInfo;
+
 /**
  * Select enemy
  * 
  * @author jabier.martinez
  */
 public class SelectEnemy extends Part {
-	
 
 	public Module bot;
-
-	
 
 	public SelectEnemy(Module bot) {
 		this.bot = bot;
 	}
 
-	
-
 	public void select() {
+		Iterator<BotInfo> iterator = bot.botsInfo.values().iterator();
+		double minDistance = Double.MAX_VALUE;
+		BotInfo selected = null;
+		while (iterator.hasNext()) {
+			BotInfo botInfo = iterator.next();
+			if ((!botInfo.teammate) && minDistance > botInfo.distance) {
+				selected = botInfo;
+				minDistance = botInfo.distance;
+			}
+		}
+		bot.enemy = selected;
 	}
-
 
 }
